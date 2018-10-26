b565462d66ebfb5acbd6edb96d31a2f7dc75a6a4
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-26 16:57:13 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,10 +8,10 @@ def sort_array_desc(array_num)
   array_num.sort do |a, b|
     b <=> a
   end
-end 
+end
 
 def sort_array_char_count(string_array)
   string_array.sort do |a, b|
-    a.length <=> b.length 
-  end 
-end 
+    a.length <=> b.length
+  end
+end
